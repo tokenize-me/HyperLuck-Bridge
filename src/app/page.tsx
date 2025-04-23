6558d9bd0dbeb3f6ae6a16f2fb75d1e28a82502f
giangcsp <giang@lockerverse.com>
2025-04-22 21:57:24 -0400

    done with hyperluck bridge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,73 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+import React from 'react';
+import WormholeConnect, { WormholeConnectConfig, nttRoutes } from '@wormhole-foundation/wormhole-connect';
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+const config: WormholeConnectConfig = {
+  network: 'Mainnet',
+  chains: ['Base', 'Solana'],
+  rpcs: {
+    'Solana': 'https://mainnet.helius-rpc.com/?api-key=919f1efa-9d9d-45fc-9706-8f25a19790e2',
+    'Base': "https://base-mainnet.g.alchemy.com/v2/BI5XlYByNdQfh3hcluwetuwR9KzTSn01"
+  },
+  tokens: ['Hyperluck'],
+  routes: [
+    ...nttRoutes({
+      tokens: {
+        HYPERLUCK_NTT: [
+          {
+            chain: 'Solana',
+            manager: "NTT82Td1AsP6tZz6UYtzZVi6FgdD4VcyutfpABF6iN8",
+            token: "5zuwrEpwbfgbgH8xRyfMdqiwHRe5VwrBPZNRxJW4pump",
+            transceiver: [
+              {
+                address: "3zXcLJeWzUfFDJJ2KeJqTyKz25uCY1yiZHA2ovXCZCVR",
+                type: 'wormhole',
+              }
+            ]
+          },
+          {
+            chain: 'Base',
+            manager: "0xBdee6ccfeB581aa7A177087565f519bD691bEC90",
+            token: "0xBB460fdf5830F80eC95793BcCC84377703F9298F",
+            transceiver: [
+              {
+                address: "0xa07398E662bF11139BC9F51CEcE09c9F0fe2AA3d",
+                type: 'wormhole'
+              }
+            ]
+          }
+        ]
+      }
+    })
+  ],
+  tokensConfig: {
+    HyperluckSol: {
+      symbol: 'Hyperluck',
+      tokenId: {
+        chain: 'Solana',
+        address: "5zuwrEpwbfgbgH8xRyfMdqiwHRe5VwrBPZNRxJW4pump"
+      },
+      icon: "https://pump.mypinata.cloud/ipfs/QmZHTED7jzzi7Yebr9bodkuhWj7ck14eDbZAkX5J87mJ7a?img-width=800&img-dpr=2&img-onerror=redirect",
+      decimals: 6
+    },
+    HyperluckBase: {
+      symbol: 'Hyperluck',
+      tokenId: {
+        chain: 'Base',
+        address: "0xBB460fdf5830F80eC95793BcCC84377703F9298F"
+      },
+      icon: "https://pump.mypinata.cloud/ipfs/QmZHTED7jzzi7Yebr9bodkuhWj7ck14eDbZAkX5J87mJ7a?img-width=800&img-dpr=2&img-onerror=redirect",
+      decimals: 18
+    }
+  }
 }
+
+const Wormhole: React.FC = () => {
+  return <WormholeConnect config={config}/>
+}
+
+export default Wormhole
+
+
+
